3b79138e68f96f96e25fe01e5c644250fd6c231d
Luigi Migliardi <luigi.migliardi@migliardi.me>
2021-10-25 15:18:42 +0200

    add attribute type in instance parameters

--- s3-synch.sh
+++ s3-synch.sh
@@ -4,7 +4,7 @@ export OLD_AWS_PROFILE=`echo $AWS_DEFAULT_PROFILE`
 echo "Setting profile"
 export AWS_DEFAULT_PROFILE=default
 echo "Copy files from extensions to s3://dev-cms-24orebs-website-static"
-aws s3 sync extensions "s3://dev-cms-24orebs-website-static" --include "*" --acl public-read
+aws s3 sync --region eu-central-1 extensions "s3://dev-cms-24orebs-website-static" --include "*" --acl public-read
 #echo "Setting permissions..."
 #aws s3 ls "s3://dev-cms-24orebs-website-static" | awk '{print $4}' | while read in; do aws s3api put-object-acl --acl public-read --bucket "dev-cms-24orebs-website-static" --key "$in"; echo Updated permission for "$in";  done
 echo "Done."
